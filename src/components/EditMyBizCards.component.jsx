fa2c0281758e2a17f6b1817ecd83f16f94ba34ec
mmk <122877863+MargoMK-wd@users.noreply.github.com>
2023-02-22 21:25:39 +0200

    Almost final project

--- src/components/EditMyBizCards.component.jsx
+++ src/components/EditMyBizCards.component.jsx
@@ -1,15 +1,9 @@
-const EditMyBizCardsComponent = ({
-  title,
-  subTitle,
-  description,
-  address,
-  phone,
-}) => {
+const EditMyBizCardsComponent = () => {
   return (
     <form>
       <div className="mb-3">
         <label htmlFor="textEditPage" className="form-label">
-          Email address
+          Email address <h1> Hola!</h1>
         </label>
         <input type="text" className="form-control" id="text-editpage" />
       </div>
